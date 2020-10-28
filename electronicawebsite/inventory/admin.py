a355960751abaf50db602e93c57bf279a96e62c8
unknown <pruebaservers@gmail.com>
2020-10-28 12:37:42 +0100

    register models, create superuser, content, advanced config admin site

--- electronicawebsite/inventory/admin.py
+++ electronicawebsite/inventory/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Category, Product, ProductInstance, Supplier
 
-# Register your models here.
+
+# admin.site.register(Product)
+# admin.site.register(ProductInstance)
+class ProductInstanceInline(admin.TabularInline):
+    model = ProductInstance
+
+
+# Register the Admin classes for Product using the decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'supplier', 'display_category')
+    inlines = [ProductInstanceInline]
+
+
+# Register the Admin classes for InstanceProduct using the decorator
+@admin.register(ProductInstance)
+class ProductInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'register_date')
+
+    fieldsets = (
+        (None, {
+            'fields': ('product', 'id'),
+        }),
+        ('Availability', {
+            'fields': ('status', 'register_date')
+        }),
+    )
+
+
+# admin.site.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('business_name', 'cif', 'business_email')
+    fields = ['business_name', ('cif', 'business_email')]
+
+
+# Register the admin class with the associated model
+admin.site.register(Supplier, SupplierAdmin)
+
+admin.site.register(Category)
+
+
--- electronicawebsite/inventory/models.py
+++ electronicawebsite/inventory/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    """Model representing a product (but not a specific copy of a book)."""
+    """Model representing a product (but not a specific copy of a product)."""
     title = models.CharField(max_length=200)
 
     # Foreing Key used because product can only have one supplier, but suppliers can have multiple products
@@ -27,17 +27,23 @@ class Product(models.Model):
     # !!! Category class has already been defined so we can specify  the object above. !!!
     category = models.ManyToManyField(Category, help_text='Select a Category for this product')
 
+    def display_category(self):
+        """Create a string for category. This is required to display category in Admin."""
+        return ', '.join(category.name for category in self.category.all()[:3])
+
+    display_category.short_description = 'Category'
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
 
     def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
+        """Returns the url to access a detail record for this product."""
         return reverse('product-detail', args=[str(self.id)])
 
 
 class ProductInstance(models.Model):
-    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    """Model representing a specific copy of a product (i.e. that can be borrowed from the library)."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular product across whole inventory')
     product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
     register_date = models.DateField(null=True, blank=True)
@@ -62,7 +68,7 @@ class ProductInstance(models.Model):
 
     def __str__(self):
         """String for representing the Model object"""
-        return f'{self.id} ({self.book.title})'
+        return f'{self.id} ({self.product.title})'
 
 
 class Supplier(models.Model):
