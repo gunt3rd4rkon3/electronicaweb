3e1bf7a747558b29cc28c5974fe226107ce8ba60
unknown <pruebaservers@gmail.com>
2020-10-31 18:39:38 +0100

    create index page: url mapping, view, templates, folders(static,tempaltes), reference static, link url, config find templates

--- electronicawebsite/inventory/views.py
+++ electronicawebsite/inventory/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Product, Supplier, ProductInstance, Category
 
-# Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_products = Product.objects.all().count()
+    num_instances = ProductInstance.objects.all().count()
+
+    # Available products (status = 'a')
+    num_instances_available = ProductInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_suppliers = Supplier.objects.count()
+
+    context = {
+        'num_products': num_products,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_suppliers': num_suppliers,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
