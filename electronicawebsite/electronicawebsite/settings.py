16862d3be6080da18b07e780e4c071895852a8f3
unknown <pruebaservers@gmail.com>
2020-11-03 11:52:50 +0100

    enable auth, create user/group, setting up auth views, show auth user

--- electronicawebsite/electronicawebsite/settings.py
+++ electronicawebsite/electronicawebsite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth',  # Core authentication framework and its default
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # email by console ?
--- electronicawebsite/electronicawebsite/urls.py
+++ electronicawebsite/electronicawebsite/urls.py
@@ -33,3 +33,7 @@ urlpatterns += [
 ]
 # Development only
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
